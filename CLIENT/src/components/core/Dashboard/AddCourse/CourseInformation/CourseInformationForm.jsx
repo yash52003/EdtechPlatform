3d936b893f3ccfa2b492a635338a0e795f00b70f
yash52003 <yashchoudhary52003@gmail.com>
2024-06-16 09:34:41 +0530

    Completed the instructor dashboard

--- CLIENT/src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ CLIENT/src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
-import { toast } from "react-hot-toast"
-import { HiOutlineCurrencyRupee } from "react-icons/hi"
-import { MdNavigateNext } from "react-icons/md"
-import { useDispatch, useSelector } from "react-redux"
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { HiOutlineCurrencyRupee } from "react-icons/hi";
+import { MdNavigateNext } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
 
 import {
   addCourseDetails,
   editCourseDetails,
   fetchCourseCategories,
-} from "../../../../../services/operations/courseDetailsAPI"
-import { setCourse, setStep } from "../../../../../slices/courseSlice"
-import { COURSE_STATUS } from "../../../../../utils/constants"
-import IconBtn from "../../../../Common/IconBtn"
-import Upload from "../Upload"
-import ChipInput from "./ChipInput"
-import RequirementsField from "./RequirementsField"
+} from "../../../../../services/operations/courseDetailsAPI";
+import { setCourse, setStep } from "../../../../../slices/courseSlice";
+import { COURSE_STATUS } from "../../../../../utils/constants";
+import IconBtn from "../../../../Common/IconBtn";
+import Upload from "../Upload";
+import ChipInput from "./ChipInput";
+import RequirementsField from "./RequirementsField";
 
 export default function CourseInformationForm() {
   const {
@@ -24,44 +24,40 @@ export default function CourseInformationForm() {
     setValue,
     getValues,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
-  const dispatch = useDispatch()
-  const { token } = useSelector((state) => state.auth)
-  const { course, editCourse } = useSelector((state) => state.course)
-  const [loading, setLoading] = useState(false)
-  const [courseCategories, setCourseCategories] = useState([])
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
+  const { course, editCourse } = useSelector((state) => state.course);
+  const [loading, setLoading] = useState(false);
+  const [courseCategories, setCourseCategories] = useState([]);
 
   useEffect(() => {
     const getCategories = async () => {
-      setLoading(true)
-      const categories = await fetchCourseCategories()
+      setLoading(true);
+      const categories = await fetchCourseCategories();
       if (categories.length > 0) {
-        // console.log("categories", categories)
-        setCourseCategories(categories)
+        setCourseCategories(categories);
       }
-      setLoading(false)
-    }
-    // if form is in edit mode
-    if (editCourse) {
-      // console.log("data populated", editCourse)
-      setValue("courseTitle", course.courseName)
-      setValue("courseShortDesc", course.courseDescription)
-      setValue("coursePrice", course.price)
-      setValue("courseTags", course.tag)
-      setValue("courseBenefits", course.whatYouWillLearn)
-      setValue("courseCategory", course.category)
-      setValue("courseRequirements", course.instructions)
-      setValue("courseImage", course.thumbnail)
-    }
-    getCategories()
+      setLoading(false);
+    };
 
+    if (editCourse) {
+      setValue("courseTitle", course.courseName);
+      setValue("courseShortDesc", course.courseDescription);
+      setValue("coursePrice", course.price);
+      setValue("courseTags", course.tag);
+      setValue("courseBenefits", course.whatYouWillLearn);
+      setValue("courseCategory", course.category);
+      setValue("courseRequirements", course.instructions);
+      setValue("courseImage", course.thumbnail);
+    }
+    getCategories();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   const isFormUpdated = () => {
-    const currentValues = getValues()
-    // console.log("changes after editing form values:", currentValues)
+    const currentValues = getValues();
     if (
       currentValues.courseTitle !== course.courseName ||
       currentValues.courseShortDesc !== course.courseDescription ||
@@ -73,42 +69,34 @@ export default function CourseInformationForm() {
         course.instructions.toString() ||
       currentValues.courseImage !== course.thumbnail
     ) {
-      return true
+      return true;
     }
-    return false
-  }
+    return false;
+  };
 
-  //   handle next button click
   const onSubmit = async (data) => {
-    // console.log(data)
-
     if (editCourse) {
-      // const currentValues = getValues()
-      // console.log("changes after editing form values:", currentValues)
-      // console.log("now course:", course)
-      // console.log("Has Form Changed:", isFormUpdated())
       if (isFormUpdated()) {
-        const currentValues = getValues()
-        const formData = new FormData()
-        // console.log(data)
-        formData.append("courseId", course._id)
+        const currentValues = getValues();
+        const formData = new FormData();
+        formData.append("courseId", course._id);
         if (currentValues.courseTitle !== course.courseName) {
-          formData.append("courseName", data.courseTitle)
+          formData.append("courseName", data.courseTitle);
         }
         if (currentValues.courseShortDesc !== course.courseDescription) {
-          formData.append("courseDescription", data.courseShortDesc)
+          formData.append("courseDescription", data.courseShortDesc);
         }
         if (currentValues.coursePrice !== course.price) {
-          formData.append("price", data.coursePrice)
+          formData.append("price", data.coursePrice);
         }
         if (currentValues.courseTags.toString() !== course.tag.toString()) {
-          formData.append("tag", JSON.stringify(data.courseTags))
+          formData.append("tag", JSON.stringify(data.courseTags));
         }
         if (currentValues.courseBenefits !== course.whatYouWillLearn) {
-          formData.append("whatYouWillLearn", data.courseBenefits)
+          formData.append("whatYouWillLearn", data.courseBenefits);
         }
         if (currentValues.courseCategory._id !== course.category._id) {
-          formData.append("category", data.courseCategory)
+          formData.append("category", data.courseCategory);
         }
         if (
           currentValues.courseRequirements.toString() !==
@@ -117,43 +105,42 @@ export default function CourseInformationForm() {
           formData.append(
             "instructions",
             JSON.stringify(data.courseRequirements)
-          )
+          );
         }
         if (currentValues.courseImage !== course.thumbnail) {
-          formData.append("thumbnailImage", data.courseImage)
+          formData.append("thumbnailImage", data.courseImage);
         }
-        // console.log("Edit Form data: ", formData)
-        setLoading(true)
-        const result = await editCourseDetails(formData, token)
-        setLoading(false)
+        setLoading(true);
+        const result = await editCourseDetails(formData, token);
+        setLoading(false);
         if (result) {
-          dispatch(setStep(2))
-          dispatch(setCourse(result))
+          dispatch(setStep(2));
+          dispatch(setCourse(result));
         }
       } else {
-        toast.error("No changes made to the form")
+        toast.error("No changes made to the form");
       }
-      return
+      return;
     }
 
-    const formData = new FormData()
-    formData.append("courseName", data.courseTitle)
-    formData.append("courseDescription", data.courseShortDesc)
-    formData.append("price", data.coursePrice)
-    formData.append("tag", JSON.stringify(data.courseTags))
-    formData.append("whatYouWillLearn", data.courseBenefits)
-    formData.append("category", data.courseCategory)
-    formData.append("status", COURSE_STATUS.DRAFT)
-    formData.append("instructions", JSON.stringify(data.courseRequirements))
-    formData.append("thumbnailImage", data.courseImage)
-    setLoading(true)
-    const result = await addCourseDetails(formData, token)
+    const formData = new FormData();
+    formData.append("courseName", data.courseTitle);
+    formData.append("courseDescription", data.courseShortDesc);
+    formData.append("price", data.coursePrice);
+    formData.append("tag", JSON.stringify(data.courseTags));
+    formData.append("whatYouWillLearn", data.courseBenefits);
+    formData.append("category", data.courseCategory);
+    formData.append("status", COURSE_STATUS.DRAFT);
+    formData.append("instructions", JSON.stringify(data.courseRequirements));
+    formData.append("thumbnailImage", data.courseImage);
+    setLoading(true);
+    const result = await addCourseDetails(formData, token);
+    setLoading(false);
     if (result) {
-      dispatch(setStep(2))
-      dispatch(setCourse(result))
+      dispatch(setStep(2));
+      dispatch(setCourse(result));
     }
-    setLoading(false)
-  }
+  };
 
   return (
     <form
@@ -296,11 +283,12 @@ export default function CourseInformationForm() {
       <div className="flex justify-end gap-x-2">
         {editCourse && (
           <button
+            type="button"
             onClick={() => dispatch(setStep(2))}
             disabled={loading}
             className={`flex cursor-pointer items-center gap-x-2 rounded-md bg-richblack-300 py-[8px] px-[20px] font-semibold text-richblack-900`}
           >
-            Continue Wihout Saving
+            Continue Without Saving
           </button>
         )}
         <IconBtn
@@ -311,5 +299,5 @@ export default function CourseInformationForm() {
         </IconBtn>
       </div>
     </form>
-  )
+  );
 }
--- CLIENT/src/components/core/Dashboard/AddCourse/RenderSteps.jsx
+++ CLIENT/src/components/core/Dashboard/AddCourse/RenderSteps.jsx
@@ -1,12 +1,13 @@
-import { FaCheck } from "react-icons/fa"
-import { useSelector } from "react-redux"
+import { FaCheck } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-import CourseBuilderForm from "./CourseBuilder/CourseBuilderForm"
-import CourseInformationForm from "./CourseInformation/CourseInformationForm"
-import PublishCourse from "./PublishCourse"
+import React from "react";
+import CourseBuilderForm from "./CourseBuilder/CourseBuilderForm";
+import CourseInformationForm from "./CourseInformation/CourseInformationForm";
+import PublishCourse from "./PublishCourse";
 
 export default function RenderSteps() {
-  const { step } = useSelector((state) => state.course)
+  const { step } = useSelector((state) => state.course);
 
   const steps = [
     {
@@ -21,23 +22,20 @@ export default function RenderSteps() {
       id: 3,
       title: "Publish",
     },
-  ]
+  ];
 
   return (
     <>
       <div className="relative mb-2 flex w-full justify-center">
         {steps.map((item) => (
-          <>
-            <div
-              className="flex flex-col items-center "
-              key={item.id}
-            >
+          <React.Fragment key={item.id}>
+            <div className="flex flex-col items-center">
               <button
                 className={`grid cursor-default aspect-square w-[34px] place-items-center rounded-full border-[1px] ${
                   step === item.id
                     ? "border-yellow-50 bg-yellow-900 text-yellow-50"
                     : "border-richblack-700 bg-richblack-800 text-richblack-300"
-                } ${step > item.id && "bg-yellow-50 text-yellow-50"}} `}
+                } ${step > item.id && "bg-yellow-50 text-yellow-50"}}`}
               >
                 {step > item.id ? (
                   <FaCheck className="font-bold text-richblack-900" />
@@ -45,39 +43,32 @@ export default function RenderSteps() {
                   item.id
                 )}
               </button>
-              
             </div>
             {item.id !== steps.length && (
-              <>
-                <div
-                  className={`h-[calc(34px/2)] w-[33%]  border-dashed border-b-2 ${
-                  step > item.id  ? "border-yellow-50" : "border-richblack-500"
-                } `}
-                ></div>
-              </>
+              <div
+                className={`h-[calc(34px/2)] w-[33%] border-dashed border-b-2 ${
+                  step > item.id ? "border-yellow-50" : "border-richblack-500"
+                }`}
+              ></div>
             )}
-          </>
+          </React.Fragment>
         ))}
       </div>
 
       <div className="relative mb-16 flex w-full select-none justify-between">
         {steps.map((item) => (
-          <>
-            <div
-              className="flex min-w-[130px] flex-col items-center gap-y-2"
-              key={item.id}
+          <div
+            className="flex min-w-[130px] flex-col items-center gap-y-2"
+            key={item.id}
+          >
+            <p
+              className={`text-sm ${
+                step >= item.id ? "text-richblack-5" : "text-richblack-500"
+              }`}
             >
-              
-              <p
-                className={`text-sm ${
-                  step >= item.id ? "text-richblack-5" : "text-richblack-500"
-                }`}
-              >
-                {item.title}
-              </p>
-            </div>
-            
-          </>
+              {item.title}
+            </p>
+          </div>
         ))}
       </div>
       {/* Render specific component based on current step */}
@@ -85,5 +76,5 @@ export default function RenderSteps() {
       {step === 2 && <CourseBuilderForm />}
       {step === 3 && <PublishCourse />}
     </>
-  )
+  );
 }
